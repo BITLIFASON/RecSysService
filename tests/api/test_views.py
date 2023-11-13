6d20e6c0ed2bd6e12ef3f35887d3321c21a31282
BITLIFASON <vladov.ru@gmail.com>
2023-11-13 20:35:17 +0300

    add test for unknown model

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from starlette.testclient import TestClient
 
 from service.settings import ServiceConfig
+from service.credentials import API_KEY
 
 GET_RECO_PATH = "/reco/{model_name}/{user_id}"
 
@@ -20,9 +21,10 @@ def test_get_reco_success(
     service_config: ServiceConfig,
 ) -> None:
     user_id = 123
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    model_name = "simple_range"
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={"Authorization": API_KEY})
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -34,8 +36,21 @@ def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
     user_id = 10**10
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    model_name = "simple_range"
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={"Authorization": API_KEY})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
+
+
+def test_get_reco_for_unknown_model(
+        client: TestClient,
+) -> None:
+    user_id = 123
+    model_name = "some_model"
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
+    with client:
+        response = client.get(path, headers={"Authorization": API_KEY})
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json()["errors"][0]["error_key"] == "model_not_found"
